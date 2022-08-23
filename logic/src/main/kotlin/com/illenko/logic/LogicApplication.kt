cdb583b035ab62d1394e0130206b02c469438f0e
illenko <kostiaillenko@gmail.com>
2022-08-23 09:52:34 +0300

    Service with business logic initial commit.

---
+++ logic/src/main/kotlin/com/illenko/logic/LogicApplication.kt
@@ -0,0 +1,11 @@
+package com.illenko.logic
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LogicApplication
+
+fun main(args: Array<String>) {
+	runApplication<LogicApplication>(*args)
+}
